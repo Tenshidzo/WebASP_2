a4808ccd113764dadeeb4ab600f8336eccdbf355
Stryzhakov Bogdan <bogdan24086@gmail.com>
2024-09-04 01:35:51 +0300

    Add project files.

---
+++ WebASP_1/Views/WebForm1.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" Debug="true" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebASP_1.Views.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Игра с матрицей кнопок</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript">
+        function generateMatrix(size) {
+            $.ajax({
+                type: "POST",
+                url: "WebForm1.aspx/GenerateMatrix",
+                data: JSON.stringify({ matrixSize: size }),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+                    $("#MatrixPanel").html(response.d.MatrixHtml);
+                    $("#ScoreLabel").text("Счёт: " + response.d.Score);
+                },
+                failure: function (response) {
+                    alert("Ошибка при генерации матрицы");
+                }
+            });
+        }
+
+        function buttonClick(buttonId) {
+            $.ajax({
+                type: "POST",
+                url: "WebForm1.aspx/ButtonClick",
+                data: JSON.stringify({ buttonId: buttonId }),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+                    $("#MatrixPanel").html(response.d.MatrixHtml);
+                    $("#ScoreLabel").text("Счёт: " + response.d.Score);
+                },
+                failure: function (response) {
+                    alert("Ошибка при обработке нажатия кнопки");
+                }
+            });
+        }
+
+        function resetGame() {
+            $.ajax({
+                type: "POST",
+                url: "WebForm1.aspx/ResetGame",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+                    $("#MatrixPanel").html("");
+                    $("#ScoreLabel").text("Счёт: 0");
+                },
+                failure: function (response) {
+                    alert("Ошибка при сбросе игры");
+                }
+            });
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="ScoreLabel" runat="server" Text="Счёт: 0" Font-Bold="True"></asp:Label>
+            <br /><br />
+            <button type="button" onclick="generateMatrix(3)">3x3</button>
+            <button type="button" onclick="generateMatrix(4)">4x4</button>
+            <button type="button" onclick="generateMatrix(5)">5x5</button>
+            <br /><br />
+            <div id="MatrixPanel"></div>
+            <br />
+            <button type="button" onclick="resetGame()">Reset</button>
+
+            <!-- Скрытые поля для хранения состояния -->
+            <asp:HiddenField ID="MatrixSizeHidden" runat="server" />
+            <asp:HiddenField ID="MatrixDataHidden" runat="server" />
+            <asp:HiddenField ID="FirstButtonValueHidden" runat="server" />
+            <asp:HiddenField ID="FirstButtonIDHidden" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebASP_1/Views/WebForm1.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebASP_1.Views
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private static int score = 0;
+        private static int[,] matrix;
+        private static string firstButtonValue;
+        private static string firstButtonId;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                score = 0;
+                matrix = null;
+                firstButtonValue = null;
+                firstButtonId = null;
+            }
+        }
+
+        [WebMethod]
+        public static object GenerateMatrix(int matrixSize)
+        {
+            matrix = GenerateMatrixWithRandomNumbers(matrixSize);
+            SaveMatrixToHiddenField(matrixSize, matrix);
+
+            string matrixHtml = DisplayMatrix(matrix);
+            return new { MatrixHtml = matrixHtml, Score = score };
+        }
+
+        private static int[,] GenerateMatrixWithRandomNumbers(int size)
+        {
+            Random rand = new Random();
+            int[,] matrix = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = rand.Next(1, 10); 
+                }
+            }
+            return matrix;
+        }
+
+        private static string DisplayMatrix(int[,] matrix)
+        {
+            StringBuilder sb = new StringBuilder();
+            int size = matrix.GetLength(0);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (matrix[i, j] != 0) // Только если значение не равно 0
+                    {
+                        string btnId = "btn_" + i + "_" + j;
+                        sb.AppendFormat("<button type='button' id='{0}' onclick='buttonClick(\"{0}\")'>{1}</button>", btnId, matrix[i, j]);
+                    }
+                }
+                sb.Append("<br />");
+            }
+            return sb.ToString();
+        }
+
+        private static void SaveMatrixToHiddenField(int size, int[,] matrix)
+        {
+            
+        }
+
+        [WebMethod]
+        public static object ButtonClick(string buttonId)
+        {
+            string[] ids = buttonId.Split('_');
+            int row = int.Parse(ids[1]);
+            int col = int.Parse(ids[2]);
+
+            if (string.IsNullOrEmpty(firstButtonValue))
+            {
+                firstButtonValue = matrix[row, col].ToString();
+                firstButtonId = buttonId;
+            }
+            else
+            {
+                int firstValue = int.Parse(firstButtonValue);
+                int secondValue = matrix[row, col];
+
+                if (firstValue + secondValue == 10)
+                {
+                    score++;
+
+                    matrix[int.Parse(firstButtonId.Split('_')[1]), int.Parse(firstButtonId.Split('_')[2])] = 0;
+                    matrix[row, col] = 0;
+                }
+
+                firstButtonValue = null;
+                firstButtonId = null;
+            }
+
+            string matrixHtml = DisplayMatrix(matrix);
+            return new { MatrixHtml = matrixHtml, Score = score };
+        }
+
+        [WebMethod]
+        public static void ResetGame()
+        {
+            score = 0;
+            matrix = null;
+            firstButtonValue = null;
+            firstButtonId = null;
+        }
+    }
+}
